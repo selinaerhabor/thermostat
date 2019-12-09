2d6b3e39eb4dd1e5467d1e35ddf08bd576602e7b
Selina Erhabor <selinaerhabor@gmail.com>
2019-12-09 14:13:21 +0000

    Added first failing test for thermostat starting at 20 degrees

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,1 +1,31 @@
+// function generateFizzBuzz(){
+//   var i = 0;
+//   while (i < 100) {
+//     i++;
+//     if (i % 3 == 0 && i % 5 == 0) {
+//       return "Fizzbuzz";
+//     } else if (i % 3 == 0) {
+//       return "Fizz";
+//     } else if (i % 5 == 0) {
+//       return "Buzz";
+//     } else {
+//       return i;
+//     }
+//   }
+// }
+// generateFizzBuzz();
 
+function generateFizzBuzz() {
+}
+
+generateFizzBuzz.prototype.number = function(number) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    return 'FizzBuzz';
+  } else if (i % 3 == 0) {
+    return 'Fizz';
+  } else if (i % 5 == 0) {
+    return 'Buzz';
+  } else {
+    return number;
+  }
+}
